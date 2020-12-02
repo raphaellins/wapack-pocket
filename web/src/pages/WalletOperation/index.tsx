5f59a102e62b51ebacfc21ef1c1a47dc3c409a3a
Raphael lins <raphalinns@gmail.com>
2020-12-02 20:20:41 -0300

    implementing the form

--- web/src/pages/WalletOperation/index.tsx
+++ web/src/pages/WalletOperation/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { Form } from "@unform/web";
 
 const Dashboard: React.FC = () => {
+  const handleSubmit = useCallback(async (data: any) => {}, []);
+
   return (
     <>
       <h1>Wallet Operation</h1>
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <input name="description" placeholder="Description"></input>
         <input name="title" placeholder="Title"></input>
         <input name="type" placeholder="Type"></input>
@@ -13,7 +16,7 @@ const Dashboard: React.FC = () => {
         <input name="amount" placeholder="Amount"></input>
         <input name="account" placeholder="Account"></input>
         <input name="date" placeholder="Date"></input>
-      </form>
+      </Form>
     </>
   );
 };
